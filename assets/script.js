2b7c8b6a71deb989c251cb9a9697d83b886d61f1
Chris <130914528+ChrisSutt@users.noreply.github.com>
2023-05-28 20:13:11 -0300

    going to bre redoing code

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 var start = document.querySelector('.start');
-var timeLeft = 10;
+var timeLeft = 60;
 var timeEl = document.querySelector('.timer');
 var startEl = document.getElementById('start');
 
@@ -16,5 +16,47 @@ start.addEventListener('click', function () {
         }, 1000);
         
 });
+
+
+
+
+document.getElementByClass('btn').addEventListener("click",function(){
+
+    var listContainer = document.getElementById("listContainer");
+    var listItem = listContainer.getElementsByTagName("li");
+
+    for (var i=0; i < listItem.length-1; i++) 
+    {
+        if(listItem[i].style.display == "list-item")
+        {
+            listItem[i].style.display = "none";
+            listItem[i+1].style.display = "list-item";
+            break;
+        }
+    }
+
+});
+
+
+
+var submitAnswer = function() {
+
+    var radios = document.getElementsByName('radio');
+    var val= "";
+    for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+            val = radios[i].value; 
+            break;
+            }
+    }
+    
+    if (val == "" ) {
+        alert('please select an answer');
+    } else if ( val == "James Gosling" ) {
+        alert('Answer is correct !');
+    } else {
+        alert('Answer is wrong');
+    }
+    };
 
 
