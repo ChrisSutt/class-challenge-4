f101d86325ac72e1719a1be2a72d10deadca0f3c
Chris <130914528+ChrisSutt@users.noreply.github.com>
2023-05-28 22:15:52 -0300

    updated all code

--- assets/script.js
+++ assets/script.js
@@ -1,62 +1,139 @@
-var start = document.querySelector('.start');
-var timeLeft = 60;
-var timeEl = document.querySelector('.timer');
-var startEl = document.getElementById('start');
-
-start.addEventListener('click', function () {
-    var timerInterval = setInterval(function() {
-        timeLeft--;
-        timeEl.textContent = timeLeft + " seconds left";
-        
-        
-        if(timeLeft === 0) {
-            clearInterval(timerInterval);
-        }
-
-        }, 1000);
-        
-});
-
-
-
-
-document.getElementByClass('btn').addEventListener("click",function(){
-
-    var listContainer = document.getElementById("listContainer");
-    var listItem = listContainer.getElementsByTagName("li");
-
-    for (var i=0; i < listItem.length-1; i++) 
+const quizData = [
     {
-        if(listItem[i].style.display == "list-item")
-        {
-            listItem[i].style.display = "none";
-            listItem[i+1].style.display = "list-item";
-            break;
-        }
+      question: "Who invented Java Programming?",
+      a: "Guido van Rossum",
+      b: "James Gosling",
+      c: "Dennis Ritchie",
+      d: "Bjarne Stroustrup",
+      correct: "b",
+    },
+    {
+      question: "Which statement is true about Jav",
+      a: "Java is a code dependent programming language",
+      b: "Java is a platform-dependent programming language",
+      c: "Java is a platform-independent programming language",
+      d: "Java is a sequence-dependent programming language",
+      correct: "d",
+    },
+    {
+      question: "Which component is used to compile, debug and execute the Java programs?",
+      a: "JRE",
+      b: "JIT",
+      c: "JDK",
+      d: "JVM",
+      correct: "c",
+    },
+    {
+      question: "Which of the following is not a Java feature?",
+      a: "Object-orientated",
+      b: "Use of pointers",
+      c: "Portable",
+      d: "Dynamic and Extinsible",
+      correct: "b",
+    },
+  ];
+
+var quiz = document.getElementById("quiz");
+var answerElements = document.querySelectorAll(".answer");
+var questionElement = document.getElementById("question");
+var atext = document.getElementById("atext");
+var btext = document.getElementById("btext");
+var ctext = document.getElementById("ctext");
+var dtext = document.getElementById("dtext");
+var submitButton = document.getElementById("submit");
+var timeLeft = 60;
+var timeEl = document.querySelector('#timer');
+var welcomePage = document.getElementById("welcome");
+var gamePage = document.getElementById("game");
+var timerInterval;
+
+var currentQuiz = 0;
+var score = 0;
+
+
+function viewWelcome()
+{
+    if(welcomePage.style.display === "none") {
+        welcomePage.style.display = "block";
     }
-
-});
-
-
-
-var submitAnswer = function() {
-
-    var radios = document.getElementsByName('radio');
-    var val= "";
-    for (var i = 0, length = radios.length; i < length; i++) {
-        if (radios[i].checked) {
-            val = radios[i].value; 
-            break;
-            }
+    else {
+        welcomePage.style.display = "none";
     }
+}
+
+function viewGame()
+{
+    if(gamePage.style.display === "none") {
+        gamePage.style.display = "block";
+    }
+    else {
+        gamePage.style.display = "none";
+    }
+}
+
+
+viewGame();
+
+
+var deselectAnswers = () => {
+    answerElements.forEach((answer) => (answer.checked = false));
+  };
+
+  var getSelected = () => {
+    var answer;
+    answerElements.forEach((answerElement) => {
+      if (answerElement.checked) answer = answerElement.id;
+    });
+    return answer;
+  };
+
+
+  function startGame() {
+    viewWelcome();
+    viewGame();
+    loadQuiz();
+    timerInterval = setInterval(gameTimer, 1000);
+  }
+
+  var loadQuiz = () => {
+    deselectAnswers();
+    var currentQuizData = quizData[currentQuiz];
+    questionElement.innerText = currentQuizData.question;
+    atext.innerText = currentQuizData.a;
+    btext.innerText = currentQuizData.b;
+    ctext.innerText = currentQuizData.c;
+    dtext.innerText = currentQuizData.d;
+  };
+  
+ 
+
+  submitButton.addEventListener("click", () => {
+    var answer = getSelected();
+    if (answer) {
+      if (answer === quizData[currentQuiz].correct) score++;
+      currentQuiz++;
+      if (currentQuiz < quizData.length) loadQuiz();
+      else {
+        quiz.innerHTML = `
+              <button onclick="history.go(0)">Play Again</button>
+        ` 
+      }
+    }
+  });
+
+
+
+function highScore()
+
+
+
+function gameTimer()
+{
+    timeLeft--;
+    timeEl.textContent = "Time: " + timeLeft;
     
-    if (val == "" ) {
-        alert('please select an answer');
-    } else if ( val == "James Gosling" ) {
-        alert('Answer is correct !');
-    } else {
-        alert('Answer is wrong');
+    if(timeLeft <= 0) {
+        clearInterval(timerInterval);
+        gameOver();
     }
-    };
-
-
+}
